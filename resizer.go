5a8a5ce3d25050063eb08cf49be65f2db0f4b2fb
Alexandre Blanquart <alex.blanquart@gmail.com>
2015-09-25 17:10:36 +0200

    resizing images concurrently + profiling

--- resizer.go
+++ resizer.go
@@ -8,51 +8,60 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/davecheney/profile"
 	"github.com/nfnt/resize"
 	"strconv"
+	"sync"
 )
 
-func resizer(target string, width uint, height uint) filepath.WalkFunc {
-	return func(img string, f os.FileInfo, err error) error {
-		if _, err := os.Stat(img); os.IsExist(err) {
-			os.Remove(img)
-			log.Printf("%s image deleted to be renewed", img)
-		}
+// Resize the JPEG image denoted by its path with specified width and height.
+// As stipulated inside resize library, one of the two can be omitted.
+// The newly JPEG resized image is in the end in the target directory with the same name.
+// In case the resized image is already present in the target directory, it is renewed by removing the old one before.
+func resizeImage(img string, target string, width uint, height uint) error {
+	// get name of image without the extra stuff to compute new path
+	name := filepath.Base(img)
+	ext := filepath.Ext(img)
+	nameWithoutExt := name[:len(name)-len(ext)]
+	newPath := target + nameWithoutExt + ".jpg"
 
-		file, err := os.Open(img)
-		defer file.Close()
-
-		initial, _, err := image.Decode(file)
-		if err != nil {
-			// Happen for root directory and images with unkown/unhandled format.
-			// For now : png and jpeg!
-			return nil
-		}
-
-		// resize to specified width and height using Lanczos resampling
-		// and preserve aspect ratio
-		resized := resize.Resize(width, height, initial, resize.Lanczos3)
-
-		// get name of image without the extra stuff to compute new path
-		name := filepath.Base(img)
-		ext := filepath.Ext(img)
-		nameWithoutExt := name[:len(name)-len(ext)]
-		newPath := target + nameWithoutExt + ".jpg"
-
-		// create the new file
-		out, err := os.Create(newPath)
-		if err != nil {
-			log.Printf("%s when creating new file", err)
-			return err
-		}
-		defer out.Close()
-
-		// write new image to file
-		return jpeg.Encode(out, resized, nil)
+	// verify image is not yet present
+	if _, err := os.Stat(newPath); os.IsExist(err) {
+		os.Remove(newPath)
+		log.Printf("%s image deleted to be renewed", newPath)
 	}
+
+	file, err := os.Open(img)
+	defer file.Close()
+	initial, _, err := image.Decode(file)
+	if err != nil {
+		// Happen for root directory and images with unkown/unhandled format
+		// Only JPEG format for now
+		return nil
+	}
+
+	// resize to specified width and height using Lanczos resampling
+	// and preserve aspect ratio
+	resized := resize.Resize(width, height, initial, resize.Lanczos3)
+
+	// create the new file
+	out, err := os.Create(newPath)
+	defer out.Close()
+	if err != nil {
+		log.Printf("%s when creating new file", err)
+		return err
+	}
+
+	// write new image to file
+	return jpeg.Encode(out, resized, nil)
 }
 
 func main() {
+	defer profile.Start(&profile.Config{
+        MemProfile: true,
+        ProfilePath: ".",
+    }).Stop()
+
 	flag.Parse()
 	if flag.NArg() != 4 {
 		log.Printf("usage : resizer directoryWithOriginalImages directoryWithResizedImages widthForResizing heightForResizing")
@@ -65,7 +74,7 @@ func main() {
 
 	log.Printf("Resizing images inside %s into %s with width %d and height %d", original, target, width, height)
 
-	// create directory if not exists
+	// create directory if not yet exist
 	if _, err := os.Stat(target); os.IsNotExist(err) {
 		err := os.Mkdir(target, 0777)
 		if err != nil {
@@ -73,5 +82,23 @@ func main() {
 		}
 	}
 
-	filepath.Walk(original, resizer(target, uint(width), uint(height)))
+	// resize images
+	var wg sync.WaitGroup
+	filepath.Walk(original, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.Mode().IsRegular() {
+			return nil
+		}
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			resizeImage(path, target, uint(width), uint(height))
+			log.Printf("%s image resized", path)
+		}()
+		return nil
+	})
+
+	wg.Wait()
 }

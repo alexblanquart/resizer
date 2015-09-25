2be1c87eb72aeb2dad85f2f886da3ce5e9ca6fba
Alexandre Blanquart <alex.blanquart@gmail.com>
2015-09-25 11:15:11 +0200

    working with the 4 arguments

--- resizer.go
+++ resizer.go
@@ -12,47 +12,44 @@ import (
 	"strconv"
 )
 
-func resizer(original string, f os.FileInfo, err error) error {
-	file, err := os.Open(original)
-	defer file.Close()
+func resizer(target string, width uint, height uint) filepath.WalkFunc {
+	return func(img string, f os.FileInfo, err error) error {
+		if _, err := os.Stat(img); os.IsExist(err) {
+			os.Remove(img)
+			log.Printf("%s image deleted to be renewed", img)
+		}
 
-	initial, _, err := image.Decode(file)
-	if err != nil {
-		// Happen for root directory and images with unkown/unhandled format.
-		// For now : png and jpeg!
-		return nil
+		file, err := os.Open(img)
+		defer file.Close()
+
+		initial, _, err := image.Decode(file)
+		if err != nil {
+			// Happen for root directory and images with unkown/unhandled format.
+			// For now : png and jpeg!
+			return nil
+		}
+
+		// resize to specified width and height using Lanczos resampling
+		// and preserve aspect ratio
+		resized := resize.Resize(width, height, initial, resize.Lanczos3)
+
+		// get name of image without the extra stuff to compute new path
+		name := filepath.Base(img)
+		ext := filepath.Ext(img)
+		nameWithoutExt := name[:len(name)-len(ext)]
+		newPath := target + nameWithoutExt + ".jpg"
+
+		// create the new file
+		out, err := os.Create(newPath)
+		if err != nil {
+			log.Printf("%s when creating new file", err)
+			return err
+		}
+		defer out.Close()
+
+		// write new image to file
+		return jpeg.Encode(out, resized, nil)
 	}
-
-	// resize to specified width and height using Lanczos resampling
-	// and preserve aspect ratio
-	resized := resize.Resize(width, height, initial, resize.Lanczos3)
-
-	// get name of image without the extra stuff to compute new path
-	name := filepath.Base(original)
-	ext := filepath.Ext(original)
-	nameWithoutExt := name[:len(name)-len(ext)]
-	newPath := target + nameWithoutExt + ".jpg"
-	log.Printf("%s image about to created", newPath)
-
-	// create the new file
-	out, err := os.Create(newPath)
-	if err != nil {
-		log.Printf("%s when creating new file", err)
-		return err
-	}
-	defer out.Close()
-
-	// write new image to file
-	return jpeg.Encode(out, resized, nil)
-	if err != nil {
-		log.Printf("%s when encoding new file as jpeg image", err)
-	}
-	return nil
-}
-
-func resize(original string, target string, width uint, height uint){
-	// call resizer on each walked file
-	filepath.Walk(original, resizer)
 }
 
 func main() {
@@ -66,12 +63,15 @@ func main() {
 	width, _ := strconv.Atoi(flag.Arg(2))
 	height, _ := strconv.Atoi(flag.Arg(3))
 
-	// renew resize directory no matter what
-	os.RemoveAll(target)
-	err := os.Mkdir(target, 0777)
-	if err != nil {
-		log.Fatal(err)
+	log.Printf("Resizing images inside %s into %s with width %d and height %d", original, target, width, height)
+
+	// create directory if not exists
+	if _, err := os.Stat(target); os.IsNotExist(err) {
+		err := os.Mkdir(target, 0777)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
-	resize(original, target, width, height)
+	filepath.Walk(original, resizer(target, uint(width), uint(height)))
 }
